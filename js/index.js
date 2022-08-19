a804d6be9afd338c943c7c8ed50f49687d8d4861
hernanrabellino <lospescau@gmail.com>
2022-08-18 23:43:17 -0300

    Added login with google

--- js/index.js
+++ js/index.js
@@ -2,22 +2,23 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-   let formulario = document.getElementById("formulario")
-
-   formulario.addEventListener('submit', validarFormulario);
-   function validarFormulario(evento) {
-    
-    let email = document.getElementById('floatingEmail').value;
-    let password = document.getElementById('floatingPassword').value;
-    if (email.length == 0 || password.length == 0) {
-        evento.preventDefault();
-        showAlertError();
-        return;
-        
-    }
-    
-   
+function redirect(){
+    window.location.href = "main.html"
 }
-});
 
+document.addEventListener("DOMContentLoaded", function () {
+    let formulario = document.getElementById("formulario")
+
+    formulario.addEventListener('submit', validarFormulario);
+    function validarFormulario(evento) {
+        console.log(evento)
+        let email = document.getElementById('floatingEmail').value;
+        let password = document.getElementById('floatingPassword').value;
+        if (email.length == 0 || password.length == 0) {
+            evento.preventDefault();
+            showAlertError();
+            return;
+
+        }
+    }
+});
